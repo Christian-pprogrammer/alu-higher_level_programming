21993e4a588de1bcc1c2c9e04377b400f5a54874
Christian-pprogrammer <mpanoc6@gmail.com>
2025-07-20 18:35:31 +0200

    fix: not a commend single select

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,13 +1,13 @@
 -- List all TV shows that are not linked to the genre 'Comedy'
 
-SELECT title
-FROM tv_shows
-WHERE id NOT IN (
-    SELECT show_id
-    FROM tv_show_genres
-    WHERE genre_id = (
-        SELECT id FROM tv_genres WHERE name = 'Comedy'
-    )
-)
-ORDER BY title ASC;
+-- List all TV shows that are not linked to the genre 'Comedy'
+
+SELECT ts.title
+FROM tv_shows ts
+LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id
+WHERE tg.name IS NULL OR tg.name != 'Comedy'
+GROUP BY ts.title
+HAVING SUM(tg.name = 'Comedy') = 0
+ORDER BY ts.title ASC;
 
