fbbcd8ff27e582665d2a311227be1852a78af9d9
Christian-pprogrammer <mpanoc6@gmail.com>
2025-07-20 18:38:33 +0200

    fix:output fix

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,12 +1,9 @@
 -- List all TV shows that are not linked to the genre 'Comedy'
 
--- List all TV shows that are not linked to the genre 'Comedy'
-
 SELECT ts.title
 FROM tv_shows ts
-LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
-LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id
-WHERE tg.name IS NULL OR tg.name != 'Comedy'
+JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+JOIN tv_genres tg ON tsg.genre_id = tg.id
 GROUP BY ts.title
 HAVING SUM(tg.name = 'Comedy') = 0
 ORDER BY ts.title ASC;
